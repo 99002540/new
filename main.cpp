fd21d0bdb746b3924500028175de70d1f24468f3
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 22:32:55 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+  #include<iostream>
+	#include "sum.h"						
+	int main()
+	{
+    printf("Hello world!\n");
+		int my_sum = sum(10, 20);				
+		printf("Sum = %d", my_sum);
+
+		
+
+		return 0;
+	}
